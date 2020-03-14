880346e63d22c06e1319ae0288994eae53eacced
Season Chen <ccqgithub@users.noreply.github.com>
2020-03-14 18:41:06 +0800

    Create mobileKeyboard.js

---
+++ js/function/mobileKeyboard.js
@@ -0,0 +1,11 @@
+// mobile keyboard
+window.addEventListener("resize", function() {
+  if(
+    document.activeElement.tagName=="INPUT"
+    || document.activeElement.tagName=="TEXTAREA"
+  ) {
+    window.setTimeout(function() {
+      document.activeElement.scrollIntoViewIfNeeded();
+    },0);
+  }
+});
