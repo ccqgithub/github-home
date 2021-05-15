6330c529a1ab847ac92843d3c92a32c9266f6ad5
Season Chen <ccqgithub@users.noreply.github.com>
2021-05-15 14:12:02 +0800

    Update useStateRef.ts

--- js/hooks/useStateRef.ts
+++ js/hooks/useStateRef.ts
@@ -1,27 +1,31 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 
 // 使用 useRef 的引用特质， 同时保持 useState 的响应性
 type StateRefObj<T> = {
+  _state: T;
   value: T;
 };
 export default function useStateRef<T>(
   initialState: T | (() => T)
 ): StateRefObj<T> {
-  const [state, setState] = useState(() => {
+  const [init] = useState(() => {
     if (typeof initialState === "function") {
       return (initialState as () => T)();
     }
     return initialState;
   });
-  const stateRef = useRef(state);
-  stateRef.current = state;
+
+  const [, setState] = useState(init);
+
   const [ref] = useState<StateRefObj<T>>(() => {
     return {
+      _state: init,
       set value(v: T) {
+        this._state = v;
         setState(v);
       },
       get value() {
-        return stateRef.current;
+        return this._state;
       },
     };
   });
