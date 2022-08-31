0aa815334c30d1ba385fb43ac180ade62bfcbe06
Season Chen <season.chen.i@foxmail.com>
2022-08-31 17:19:01 +0800

    Update debounce.ts

--- js/function/debounce.ts
+++ js/function/debounce.ts
@@ -1,22 +1,26 @@
-/**
- * 函数调用间隔控制
- * fn: 执行的函数
- * delay: 函数执行的最小间隔，单位毫秒
- * prepose：靠前执行。为true时，函数调用即执行，并且在之后的delay中的调用无效（应用场景：防止按钮多次重复点击）。为false时，函数调用之后，并不立即执行，只有某次调用之后的delay内没有再被调用，才执行（应用场景：autocomplete，只有输入完成之后再执行查询）。
- */
-export default function debounce(fn: (...args: any[]) => void, delay: number, prepose = false) {
-  return function (...args: any[]) {
-    let timer;
-    let last_exec = -Infinity;
-    const exec = function () {
+export const debounce = <F extends (...args: any[]) => void>(
+  fn: F,
+  delay: number,
+  opts?: {
+    leading?: boolean;
+  }
+): F => {
+  const { leading = false } = opts || {};
+  let timer: ReturnType<typeof setTimeout> | null = null;
+  let last_exec: number | null = null;
+
+  const func = (...args: any[]) => {
+    const exec = () => {
       last_exec = Date.now();
-      fn.apply(this, ...args);
+      fn(...args);
     };
     timer && clearTimeout(timer);
-    if (prepose) {
-      Date.now() - last_exec >= delay && exec();
-    } else {
-      timer = setTimeout(exec, delay);
+    if (leading && !last_exec) {
+      exec();
+      return;
     }
+    timer = setTimeout(exec, delay);
   };
-}
+
+  return func as F;
+};
